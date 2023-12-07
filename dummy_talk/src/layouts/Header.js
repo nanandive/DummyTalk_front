8e2b77a9b2a78090507a8b5c209043ad01b09d2e
kimjisu123 <dkljjh10@gmail.com>
2023-12-07 13:42:02 +0900

    중간커밋

--- dummy_talk/src/layouts/Header.js
+++ dummy_talk/src/layouts/Header.js
@@ -2,7 +2,14 @@ function Header(){
 
     return (
         <>
-            <div className="h-[77px] border border-black ">
+            <div style={{display:"flex", height:"77px", border:"1px solid black"}}>
+                <div style={{border:"1px solid black", width:"130px", marginRight:"130px", borderRedius:"10px"}}>
+
+                </div>
+                <div style={{border:"1px solid black", width:"130px", marginRight:"300px"}}>
+
+                </div>
+
             </div>
         </>
     )
