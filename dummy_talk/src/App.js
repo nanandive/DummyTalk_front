152e27c4e5c82c901a76a279cbe319a22993d4f7
DESKTOP-A9G14KA\Admin <dkljjh10@gmail.com>
2023-12-07 09:44:03 +0900

    commit

--- dummy_talk/src/App.js
+++ dummy_talk/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/layout";
+import MainPage from "./page/MainPage";
 
 function App() {
     return (
     <BrowserRouter>
         <Routes>
             <Route path='main' element={<Layout/>}>
+                <Route index element={<MainPage />}>
+                    {/* 추후 순서대로 Left, Chat, Right를 넣으면 됨 */}
+                </Route>
             </Route>
         </Routes>
     </BrowserRouter>
--- dummy_talk/src/page/MainPage.js
+++ dummy_talk/src/page/MainPage.js
@@ -1,13 +1,17 @@
 import RightBar from "./RightBar";
 import LeftBar from "./LeftBar";
 import Chat from "./Chat";
+import {Outlet} from "react-router-dom";
 
 function MainPage() {
 
     return (
         <>
             <div style={{display : "flex"}}>
-                <LeftBar />
+                {/* 추후 outlet 사용 밑은 테스트*/}
+                {/*<Outlet />*/}
+
+                <LeftBar /> 
                 <Chat />
                 <RightBar />
             </div>
