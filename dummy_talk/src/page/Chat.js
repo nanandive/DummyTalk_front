06b764b3a858e61d2ebb8108a43ca45198cd964b
haeunyy <ibbihe@gmail.com>
2023-12-07 15:18:09 +0900

    채팅 레이아웃 작성중

--- dummy_talk/src/page/Chat.js
+++ dummy_talk/src/page/Chat.js
@@ -1,57 +1,26 @@
-import { ScrollArea } from "@radix-ui/react-scroll-area";
-import { ScrollBar } from "src/components/ui/scroll-area";
+import { ScrollArea } from "src/components/ui/scroll-area";
 
 function Chat() {
 
     return (
         <div className="flex w-full flex-col h-full">
             <div className="bg-amber-500 h-[10%] p-6">서브방 이름</div>
-            <ScrollArea className="h-[70%] w-full rounded-md border overflow-scroll">
-                {/* <ScrollBar className=""/> */}
+            <ScrollArea className="h-[70%] w-full rounded-md border">
                 <div className="h-[70%] p-5 flex flex-col mt-auto">
                     <div className='w-full box-border h-32 p-4 border-4'>
                     채팅 텍스트 1
                     </div>
-                    <div className='w-full box-border h-32 p-4 border-4'>
-                    채팅 텍스트 1
+                    <div className=""></div>
+                    <div className="">
+                        right
+                        <div className="">date</div>
+                        <div className="">text</div>
                     </div>
-                    <div className='w-full box-border h-32 p-4 border-4'>
-                    채팅 텍스트 1
-                    </div>
-                    <div className='w-full box-border h-32 p-4 border-4'>
-                    채팅 텍스트 1
-                    </div>
-                    <div className='w-full box-border h-32 p-4 border-4'>
-                    채팅 텍스트 1
-                    </div>
-                    <div className='w-full box-border h-32 p-4 border-4'>
-                    채팅 텍스트 1
-                    </div>
-                    <div className='w-full box-border h-32 p-4 border-4'>
-                    채팅 텍스트 1
-                    </div>
-                    <div className='w-full box-border h-32 p-4 border-4'>
-                    채팅 텍스트 1
-                    </div>
-                    <div className='w-full box-border h-32 p-4 border-4'>
-                    채팅 텍스트 1
-                    </div>
-                    <div className='w-full box-border h-32 p-4 border-4'>
-                    채팅 텍스트 1
-                    </div>
-                    <div className='w-full box-border h-32 p-4 border-4'>
-                    채팅 텍스트 1
-                    </div>
-                    <div className='w-full box-border h-32 p-4 border-4'>
-                    채팅 텍스트 1
-                    </div>
-                    <div className='w-full box-border h-32 p-4 border-4'>
-                    채팅 텍스트 1
-                    </div>
+
                 </div>
             </ScrollArea >
-            <div className='message_area h-[20%]'>
-                메세지
+            <div className='message_area h-[22%]'>
+                
             </div>
         </div>
     )
