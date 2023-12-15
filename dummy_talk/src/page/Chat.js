b5ed3f88988441f6126d97cc93f287ab9460dbeb
haeunyy <ibbihe@gmail.com>
2023-12-15 18:15:18 +0900

    채팅 send 시점 수정중

--- dummy_talk/src/page/Chat.js
+++ dummy_talk/src/page/Chat.js
@@ -1,6 +1,6 @@
 import {Switch} from "@headlessui/react";
 import {ChevronLeft, ChevronsLeft, ChevronsRight, icons, ImagePlus} from "lucide-react";
-import {useEffect, useState} from "react";
+import {useEffect, useMemo, useState} from "react";
 import ChatItem from "src/components/chat/chat-item";
 import {Button} from "src/components/ui/button";
 import {Label} from "src/components/ui/label";
@@ -11,24 +11,31 @@ import Stomp from 'webstomp-client';
 import {useUrlQuery} from "src/components/hooks/use-url-query";
 import axios from "axios";
 import ChatEmpty from "src/components/chat/ChatEmpty";
-import {json} from "react-router-dom";
+import {decodeJwt} from "src/util/tokenUtils";
 
 function Chat({isOpen, setOpen}) {
+
+    const {onOpen, onClose} = useModal()
     const query = useUrlQuery()
-    const [channelId, setChannel] = useState(1);
-    // const channelId = query.get("channel")
-    // isOpen, setOpen 오른쪽 사이드바
+    const channelId = query.get("channel")
+
+    const accessToken = localStorage.getItem("accessToken")
+    const userId = useMemo(() => decodeJwt(accessToken).sub, [accessToken]);
 
     const [enabled, setEnabled] = useState(false); // 채팅번역 기능
     const [data, setData] = useState([]);
-    const {onOpen, onClose} = useModal();
     const [sendMessage, setSendMessage] = useState('');
+
+    const SOCKET_HOST = new SockJS('http://localhost:9999/websocket');
+    const socket = Stomp.over(SOCKET_HOST);
 
     // 메시지를 입력할 때마다 메시지를 업데이트
     const handleChange = (e) => {
         console.log(e);
         setSendMessage(e.target.value);
     };
+    const WEBSOCKLOGIN = `/app/${channelId}/message`;
+
 
     // 엔터키 눌렀을 때 메시지 전송
     const enter_event = (e) => {
@@ -40,6 +47,7 @@ function Chat({isOpen, setOpen}) {
             // 메시지를 전송한 후에 메시지를 초기화
             setSendMessage('');
             console.log(sendMessage);
+
         }
     }
 
@@ -54,7 +62,7 @@ function Chat({isOpen, setOpen}) {
         try {
             const response = await axios.get(`http://localhost:9999/channel/chat/${channelId}`);
             setData(response.data.data);
-            console.log("===================================== response "+ response)
+            console.log("===================================== response " + response)
         } catch (error) {
             console.error("채팅 리스트 뽑아보기 에러", error);
         }
@@ -78,191 +86,129 @@ function Chat({isOpen, setOpen}) {
      * subscribe : /topic/msg/{channelId}
      * send : /app/{channelId}/message
      */
+
     // stomp 옵션 설정
-    const options = {
-            // protocols : ['v12.stomp', 'v11.stomp'],
-            // binary : false,             // true : 바이너리 값 사용 가능
-            // heartbeat : [10000, 10000], // [서버->클라이언트, 클라이언트->서버] ms
-            debug : false               // true : 디버깅 모드
+    useEffect(() => {
+
+
+        // const USERREPLY = `/topic/msg/${channelId}`;
+        //
+        //
+        // console.log('socket =====================================: ' + socket)
+        // socket.subscribe(USERREPLY, function (msg) {
+        //     console.log('msg : ' + msg);
+        // });
+        // socket.send(WEBSOCKLOGIN, onMessage());
+        //
+        // setSendMessage('');
+
+    }, []);
+
+
+    const onMessage = (msg) => {
+        console.log('onMessage =====================================: ' + msg)
+        // return JSON.stringify({
+        //     message: sendMessage,
+        //     'sender': userId,
+        //     language: 'en',
+        //     channelId
+        // });
     }
-    console.log('socket =====================================: ' + socket)
-    socket.connect({}, function (frame) {
-        console.log('Connected: ' + frame);
 
-        socket.subscribe(USERREPLY, function (msg) {
-            console.log('msg : ' + msg);
-        });
+    const onError= (e) => {
+        const error = e.body;
+        console.log('Alarm Websocket - Error: ', error)
+        throw error;
+    }
 
-        socket.send(WEBSOCKLOGIN, {}, JSON.stringify({
-            message: {
-                sender: 1,
-                message: sendMessage,
-                language: "ko",
-                channelId: 1
-            }
-        }));
-    }, options);
+    const USERREPLY = `/topic/msg/${channelId}`;
 
-        // const [subscribe, setubscribe] = useState([]);
+    const onConnection = () => {
+        console.log('onConnected =====================================: ' + socket)
 
+        socket.subscribe(USERREPLY, onMessage);
+        socket.send(WEBSOCKLOGIN, sendMessage);
+    }
 
+    socket.connect({}, onConnection, onError);   // 소켓 연결
 
-        // const onConnected  = (frame) => {
-        //     console.log('Connected ===================================== ' + frame);
-        //
-        //     socket.subscribe('/topic/msg', function (msg) {
-        //         console.log('msg : ' + msg);
-        //         const newMessage = JSON.parse(msg.body);
-        //         setMessages((prevMessages) => [...prevMessages, newMessage]);
-        //     });
-        //
-        //     // return () => {
-        //     //     // Cleanup and disconnect websocket when component unmounts
-        //     //     socket.disconnect();
-        //     // };
-        // }
+    if (!channelId) return null;
 
-        // useEffect(() => {
-        //
-        //     const SOCKET_HOST = 'http://localhost:9999/websocket';
-        //     const socket = Stomp.over(new SockJS(SOCKET_HOST));
-        //
-        //
-        //     console.log('socket =====================================: ' + socket)
-        //
-        //     socket.connect({}, function (frame) {
-        //         console.log('Connected ===================================== frame: ' + frame);
-        //         socket.subscribe(USERREPLY, function (msg) {
-        //             console.log('msg : ' + msg);
-        //             // setMessages((prevMessages) => [...prevMessages, newMessage]);
-        //             // stompClient.disconnect();
-        //         });
-        //         // socket.subscribe(WEBSOCKLOGIN);
-        //         socket.send(WEBSOCKLOGIN, {
-        //             sender: 1,
-        //             message: sendMessage,
-        //             language: "ko",
-        //             channelId: 1
-        //         });
-        //     });
-        //     const onMessage = (msg) => {
-        //         console.log('msg =====================================: ', msg);
-        //         const newMessage = JSON.parse(msg.body);
-        //         setMessages((prevMessages) => [...prevMessages, newMessage]);
-        //     }
-        //
-        //     // const onConnected = () => {
-        //     //     console.log('Connected ===================================== ');
-        //     //     socket.subscribe(USERREPLY, onMessage);
-        //     //     socket.subscribe(WEBSOCKLOGIN);
-        //     //     socket.send(WEBSOCKLOGIN, {
-        //     //             "sender": 1,
-        //     //             "message": "test message",
-        //     //             "language": "ko",
-        //     //             "channelId": 1
-        //     //         }
-        //     //     );
-        //     // }
-        //     const onError = (err) => console.error('Alarm Websocket - Error', err);
-        //
-        //     // message: {'"sender": 1,"message": "test message","language": "ko"}',channelId: 1});
-        //     // socket.connect({}, function (frame) {
-        //     //     console.log('Connected ===================================== ');
-        //     //     socket.subscribe(USERREPLY, onMessage);
-        //     //     socket.subscribe(WEBSOCKLOGIN);
-        //     //     socket.send(WEBSOCKLOGIN, {
-        //     //         sender: 1,
-        //     //         message: sendMessage,
-        //     //         language: "ko",
-        //     //         channelId: 1
-        //     //     } );
-        //     // }, onError);
-        //
-        //     return () => {
-        //         // Cleanup 함수에서 disconnect를 호출하여 컴포넌트가 unmount 될 때 WebSocket을 닫을 수 있도록 합니다.
-        //         console.log('Disconnected ===================================== ');
-        //         socket.disconnect();
-        //     };
-        // }, []); // channelId가 변경될 때마다 useEffect가 다시 실행
-
-
-        // socket.send(`/app/message`, {"message": message, "channelId": channelId}, JSON.stringify({}));
-
-        return (
-            data.length === 0 ? <ChatEmpty/> : (
-                <div className="flex w-full flex-col h-full">
-                    {/* 채널명 */}
-                    <div
-                        className="h-[50px] font-bold text-xl flex pl-5 items-center bg-[#D9D9D9] border-y-[1px] border-black justify-between">
-                        <div>
-                            서브방 이름
-                        </div>
-                        {/* 우측 사이드 닫힘 / 열림 */}
-                        <Button variant={"icon"} onClick={() => setOpen(prev => !prev)}>
-                            {isOpen ? <ChevronsRight/> : <ChevronsLeft/>}
-                        </Button>
-
+    return (
+        data.length === 0 ? <ChatEmpty/> : (
+            <div className="flex w-full flex-col h-full">
+                {/* 채널명 */}
+                <div
+                    className="h-[50px] font-bold text-xl flex pl-5 items-center bg-[#D9D9D9] border-y-[1px] border-black justify-between">
+                    <div>
+                        서브방 이름
                     </div>
-                    {/* 채팅방 스크롤 바 구역 */}
-                    <div className="h-3/4 flex items-end ml-3 overflow-y-auto scrollbar-hidden">
-                        <div className="h-full w-full">
-                            {data.map((chat) => (
-                                <ChatItem
-                                    key={chat.chatId}
-                                    content={chat.message}
-                                    member={chat.sender}
-                                    timestamp={"20200"}
-                                />
-                            ))}
-                        </div>
-                    </div>
-                    {/* 메시지 입력 */}
-                    <div className="flex flex-col h-1/4 relative overflow-hidden px-5 py-2 rounded-lg">
-                        {/* 채팅 번역 스위치 */}
-                        <div className="flex flex-row-reverse pb-2">
-                            <Label
-                                htmlFor="airplane-mode"
-                                className="font-bold text-2 self-center "
-                            >
-                                채팅번역
-                            </Label>
-                            <Switch
-                                checked={enabled}
-                                onChange={setEnabled}
-                                className={`${
-                                    enabled ? "bg-yellow-400 mr-1" : "bg-gray-400 mr-1"
-                                } relative inline-flex h-[25px] w-[50px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white/75`}
-                            >
-                                <span className="sr-only">Use setting</span>
-                                <span
-                                    aria-hidden="true"
-                                    className={`${
-                                        enabled ? "translate-x-6" : "translate-x-0"
-                                    } pointer-events-none inline-block h-[21px] w-[21px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
-                                />
-                            </Switch>
-                        </div>
-                        {/* 메시지 입력란 */}
-                        <Textarea className="w-full h-full resize-none top-3 outline outline-zinc-300"
-                                  maxLength="500"
-                                  onChange={handleChange}
-                                  onKeyPress={enter_event}
-                                  value={sendMessage}
-                                  placeholder="메시지를 입력하세요."/>
-                        <div className="absolute right-[5%] bottom-[10%] ">
-                            {/* 사진 전송 버튼 */}
-                            <Button
-                                className="place-self-center"
-                                onClick={() => onOpen('imageSend')}>
-                                <ImagePlus/>
-                            </Button>
-                            {/* 메시지 전송 버튼 */}
-                            <Button className="h-8 bg-sky-600 text-white">Send</Button>
-                        </div>
+                    {/* 우측 사이드 닫힘 / 열림 */}
+                    <Button variant={"icon"} onClick={() => setOpen(prev => !prev)}>
+                        {isOpen ? <ChevronsRight/> : <ChevronsLeft/>}
+                    </Button>
+
+                </div>
+                {/* 채팅방 스크롤 바 구역 */}
+                <div className="h-3/4 flex items-end ml-3 overflow-y-auto scrollbar-hidden">
+                    <div className="h-full w-full">
+                        {data.map((chat) => (
+                            <ChatItem
+                                key={chat.chatId}
+                                content={chat.message}
+                                member={chat.sender}
+                                timestamp={"20200"}
+                            />
+                        ))}
                     </div>
                 </div>
-            )
-        );
+                {/* 메시지 입력 */}
+                <div className="flex flex-col h-1/4 relative overflow-hidden px-5 py-2 rounded-lg">
+                    {/* 채팅 번역 스위치 */}
+                    <div className="flex flex-row-reverse pb-2">
+                        <Label
+                            htmlFor="airplane-mode"
+                            className="font-bold text-2 self-center "
+                        >
+                            채팅번역
+                        </Label>
+                        <Switch
+                            checked={enabled}
+                            onChange={setEnabled}
+                            className={`${
+                                enabled ? "bg-yellow-400 mr-1" : "bg-gray-400 mr-1"
+                            } relative inline-flex h-[25px] w-[50px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white/75`}
+                        >
+                            <span className="sr-only">Use setting</span>
+                            <span
+                                aria-hidden="true"
+                                className={`${
+                                    enabled ? "translate-x-6" : "translate-x-0"
+                                } pointer-events-none inline-block h-[21px] w-[21px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+                            />
+                        </Switch>
+                    </div>
+                    {/* 메시지 입력란 */}
+                    <Textarea className="w-full h-full resize-none top-3 outline outline-zinc-300"
+                              maxLength="500"
+                              onChange={handleChange}
+                              onKeyPress={enter_event}
+                              value={sendMessage}
+                              placeholder="메시지를 입력하세요."/>
+                    <div className="absolute right-[5%] bottom-[10%] ">
+                        {/* 사진 전송 버튼 */}
+                        <Button
+                            className="place-self-center"
+                            onClick={() => onOpen('imageSend')}>
+                            <ImagePlus/>
+                        </Button>
+                        {/* 메시지 전송 버튼 */}
+                        <Button className="h-8 bg-sky-600 text-white">Send</Button>
+                    </div>
+                </div>
+            </div>
+        )
+    );
 }
 
 
