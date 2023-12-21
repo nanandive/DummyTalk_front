cfb7b86649ff673dc6fa6117792f518b9574f08b
heski0709 <heski0709@gmail.com>
2023-12-21 21:28:34 +0900

    token 검증 전에 Layout이 Paint되는 오류 수정

--- src/components/providers/AuthProvider.js
+++ src/components/providers/AuthProvider.js
@@ -1,45 +1,37 @@
-import {Outlet, useNavigate} from "react-router-dom";
-import {useEffect} from "react";
-import {jwtDecode} from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
+import { Navigate, Outlet, useNavigate } from "react-router-dom";
 
 export const AuthProvider = () => {
-
-
     const navigate = useNavigate();
 
-    const accessToken = window.localStorage.getItem('accessToken');
+    const accessToken = window.localStorage.getItem("accessToken");
     const decodedToken = accessToken ? jwtDecode(accessToken) : null;
 
+    if (accessToken === null) {
+        alert("로그인이 필요합니다.");
+        navigate("/sign-up", { replace: true });
 
-    useEffect(() => {
+        return <Navigate to={"/sign-up"} replace={true} />
+    }
+        
+    const verified = TokenExpiration(decodedToken);
 
-        if (accessToken === null) {
-            alert('로그인이 필요합니다.');
-            navigate("/sign-up", {replace: true});
-        } else {
+    if (verified === false) {
+        alert("로그인 세션시간이 만료되었습니다.");
+        
+        return <Navigate to={"/sign-up"} replace={true} />
+    }
+    
 
-            const verified = TokenExpiration(decodedToken);
-
-            if (verified === false) {
-                alert('로그인 세션시간이 만료되었습니다.');
-                navigate("/sign-up", {replace: true});
-                return null;
-            }
-        }
-
-    }, [])
-
-
-    return <Outlet/>
-}
+    return <Outlet />;
+};
 
 const TokenExpiration = (decodedToken) => {
-
-    if (decodedToken.exp*1000 < Date.now()) {
-        window.localStorage.removeItem("accessToken")
+    if (decodedToken.exp * 1000 < Date.now()) {
+        window.localStorage.removeItem("accessToken");
         return false;
-    }
-    else {  // 엑세스 만료되지 않음
+    } else {
+        // 엑세스 만료되지 않음
         return true;
     }
 };
