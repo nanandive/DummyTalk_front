4a751cbd30a91dba587c4760e0ad00f05d60b0a7
heski0709 <heski0709@gmail.com>
2023-12-22 18:36:59 +0900

    회원 정보 수정 modal fix

--- src/components/modals/user-modal.jsx
+++ src/components/modals/user-modal.jsx
@@ -24,14 +24,16 @@ function UserModal() {
                     <DialogTitle className="text-2xl text-center font-bold">
                         회원정보 수정
                     </DialogTitle>
-                    <DialogDescription className="text-center text-zinc-500">
-                        <div className='flex gap-1 justify-between'>
-                            {/* 정보 수정*/}
-                            <div className="flex flex-col gap-2 w-4/5 justify-between items-center">
+                    <DialogDescription className="text-center text-zinc-500"></DialogDescription>
+                </DialogHeader>
+                <div className="flex gap-1 flex-col justify-between">
+                    {/* 정보 수정*/}
+                    <div className="flex flex-col gap-2 items-center">
+                        <div className="flex items-center justify-between w-full px-4">
+                            <div className='gap-3 flex flex-col'>
                                 <input
                                     type={"text"}
                                     placeholder={"변경할 닉네임"}
-                                    
                                 />
                                 <input
                                     type={"text"}
@@ -41,38 +43,44 @@ function UserModal() {
                                     type={"text"}
                                     placeholder={"변경할 비밀번호 확인"}
                                 />
-                                <select
-                                    placeholder={"국가선택"}
-                                    className="pl-1 border border-[#F1F1F1] w-[250px] h-[30px]  rounded-md"
-                                >
-                                    <option
-                                        value="국가선택"
-                                        disabled
-                                        selected
-                                        hidden
-                                    >
-                                        국가선택
-                                    </option>
-                                    <option value="국가1">국가1</option>
-                                    <option value="국가2">국가2</option>
-                                    <option value="국가3">국가3</option>
-                                </select>
                             </div>
-                            {/* 이미지 등록 및 확인 버튼*/}
-                            <div className='flex flex-col justify-between'>
+
+                            <div>
                                 <img src="./image 29.png"></img>
-                                <Button className='hover:bg-amber-500 bg-amber-400 font-semibold text-sm text-black'>
-                                    수정
-                                </Button>
-                                <Button className='bg-red-400 hover:bg-red-500 font-semibold text-sm text-black' onClick={onClose}>
-                                    취소
-                                </Button>
                             </div>
                         </div>
-                    </DialogDescription>
-                </DialogHeader>
+                        </div>
+
+                        <select
+                            placeholder={"국가선택"}
+                            className="pl-1 border border-[#F1F1F1] w-[250px] h-[30px]  rounded-md"
+                        >
+                            <option
+                                value="국가선택"
+                                disabled
+                                selected
+                                hidden
+                            >
+                                국가선택
+                            </option>
+                            <option value="국가1">국가1</option>
+                            <option value="국가2">국가2</option>
+                            <option value="국가3">국가3</option>
+                        </select>
+                    {/* 이미지 등록 및 확인 버튼*/}
+                </div>
                 <DialogFooter>
-                    
+                    <div className="flex gap-3 justify-between">
+                        <Button className="hover:bg-amber-500 bg-amber-400 font-semibold text-sm text-black">
+                            수정
+                        </Button>
+                        <Button
+                            className="bg-red-400 hover:bg-red-500 font-semibold text-sm text-black"
+                            onClick={onClose}
+                        >
+                            취소
+                        </Button>
+                    </div>
                 </DialogFooter>
             </DialogContent>
         </Dialog>
