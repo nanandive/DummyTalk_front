c8af6bb06f6e841382365f0e53e1e521c77c5f22
heski0709 <heski0709@gmail.com>
2023-12-27 19:10:29 +0900

    socket에 sessionId 추가

--- src/components/providers/socket-provider.js
+++ src/components/providers/socket-provider.js
@@ -4,24 +4,33 @@ import Stomp from "webstomp-client";
 import { useSocket } from "../hooks/use-socket";
 
 export const SocketProvider = ({ children }) => {
-  // const [socket, setSocket] = useState(null);
-  // const [isConnected, setIsConnected] = useState(false);
-  const { setSocket, isConnected } = useSocket();
-  const accessToken = localStorage.getItem("accessToken");
+    // const [socket, setSocket] = useState(null);
+    // const [isConnected, setIsConnected] = useState(false);
+    const { setSocket } = useSocket();
+    const accessToken = localStorage.getItem("accessToken");
 
-  useEffect(() => {
-    if (!accessToken) return;
+    useEffect(() => {
+        if (!accessToken) return;
 
-    const sockJs = new SockJS(`${process.env.REACT_APP_API_URL}/websocket`);
-    const stomp = Stomp.over(sockJs, { debug: false });
+        const sockJs = new SockJS(`${process.env.REACT_APP_API_URL}/websocket`);
+        const stomp = Stomp.over(sockJs, { debug: false });
 
-    stomp.connect({}, function (frame) {
-      console.log("Connected: " + frame);
-      setSocket(stomp);
-    });
+        stomp.connect({}, function (frame) {
+            console.log("Connected: " + frame);
+            let url = stomp.ws._transport.url;
+            url = url.replace(
+                "ws://localhost:9999",
+                ""
+            );
+            url = url.replaceAll("/websocket", "");
+            url = url.replace(/^\/[0-9]+\//, "");
+            stomp.id = url;
+            
+            setSocket(stomp);
+        });
 
-    return () => stomp.disconnect(() => {});
-  }, [accessToken]);
+        return () => stomp.disconnect(() => {});
+    }, [accessToken]);
 
-  return <>{children}</>;
+    return <>{children}</>;
 };
