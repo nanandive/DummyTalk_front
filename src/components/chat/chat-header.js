7da2489019209855af6ed7c64b211fcfbd5f0292
haeunyy <ibbihe@gmail.com>
2023-12-27 18:22:41 +0900

    ui 변경

--- src/components/chat/chat-header.js
+++ src/components/chat/chat-header.js
@@ -1,7 +1,7 @@
-import { ChevronsLeft, ChevronsRight } from "lucide-react";
+import {ChevronsLeft, ChevronsRight, UserPlus} from "lucide-react";
 import { Button } from "../ui/button";
 import axios from "axios";
-import {useEffect, useState} from "react";
+import { useEffect, useState} from "react";
 import {useUrlQuery} from "src/components/hooks/use-url-query";
 
 const ChatHeader = ({ isOpen, setOpen }) => {
@@ -28,7 +28,7 @@ const ChatHeader = ({ isOpen, setOpen }) => {
     }, [channelId]);
 
     return channelName && (
-        <div className="h-[50px] font-bold text-xl flex pl-5 items-center bg-[#D9D9D9] border-y-[1px] border-black justify-between">
+        <div className="h-[50px] font-bold text-xl flex pl-5 items-center bg- border-y-[1px] border-black justify-between text-[#B5BAC1]">
             <div>{channelName}</div>
             {/* 우측 사이드 닫힘 / 열림 */}
             <Button
