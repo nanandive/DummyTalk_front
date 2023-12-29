9ddf27433639f697d3b89fc389bb441dcc1df7ef
kimjisu123 <dkljjh10@gmail.com>
2023-12-29 12:55:10 +0900

    커밋

--- src/api/chat/ChatAPICalls.js
+++ src/api/chat/ChatAPICalls.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import {POST_REFUSAL} from "modules/MainModule";
+import {callGetFriendRequest} from "api/MainAPICalls";
 
 
     // 메시지 리스트
@@ -13,6 +15,24 @@ const fetchChatData = async () => {
         console.error("채팅 리스트 뽑아보기 에러", error);
     }
 }
+
+const chatEmbedding = (friendId) => {
+    const requestURL = `${process.env.REACT_APP_API_URL}/refusal/${decodedToken.sub}`;
+    return async (dispatch, getState) => {
+        const result = await fetch(requestURL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': '*/*'
+            },body: JSON.stringify(friendId)
+        }).then(response => response.json());
+        alert(result.message)
+        dispatch(callGetFriendRequest())
+        dispatch({ type: POST_REFUSAL, payload: result.data });
+    }
+}
+
+
 
 
 export default fetchChatData;
