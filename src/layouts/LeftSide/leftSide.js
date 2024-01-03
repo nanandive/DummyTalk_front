e8f14c1242338f820789c40762f83c3e738a302f
heski0709 <heski0709@gmail.com>
2024-01-03 21:35:47 +0900

    server 데이터를 가져오도록 수정

--- src/layouts/LeftSide/leftSide.js
+++ src/layouts/LeftSide/leftSide.js
@@ -1,3 +1,6 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import Channels from "src/components/channel/Channel";
 import Friends from "src/components/channel/Friends";
 import ChannelHeader from "src/components/channel/channel-header";
@@ -8,12 +11,29 @@ import { Separator } from "src/components/ui/separator";
 function LeftSide() {
     const query = useUrlQuery();
     const serverId = query.get("server");
-    if (!serverId) return null;
+    const [serverData, setServerData] = useState(null);
+    const { state } = useLocation();
+    useEffect(() => {
+        const fetchServerDetail = async () => {
+            try {
+                const response = await axios.get(
+                    `${process.env.REACT_APP_API_URL}/server/${serverId}`
+                );
+                setServerData(response.data);
+            } catch (error) {
+                console.log("서버 불러오기 실패", error);
+            }
+        };
+
+        fetchServerDetail();
+    }, [serverId, state]);
+
+    if (!serverId || !serverData) return null;
 
     return (
-        <section className="w-60 min-w-60 h-full bg-[#112033] flex flex-col">
-            <ChannelHeader />
-            <Channels />
+        <section className="w-60 min-w-[240px] h-full bg-[#112033] flex flex-col">
+            <ChannelHeader server={serverData} />
+            <Channels server={serverData} />
             <Separator className="my-4 bg-black" />
             <Friends className=" overflow-y-scroll" />
             <Separator className="my-4 bg-black" />
