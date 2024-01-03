6fe8f742121c5b1fa359b6006b1bc81ee9ab4179
Choi Youngwook <duddnr3618@gmail.com>
2024-01-03 19:04:01 +0900

    24.01.03 rightbar commit

--- src/layouts/MainLayout/RightBar.js
+++ src/layouts/MainLayout/RightBar.js
@@ -6,6 +6,7 @@ import {Button} from "src/components/ui/button";
 import CellComponent from "src/layouts/RightSide/component/CellComponent";
 import TextSearchComponent from "src/layouts/RightSide/component/text-search-component";
 import SummaryComponent from "src/layouts/RightSide/component/summary-component";
+import {useSocket} from "src/components/hooks/use-socket";
 
 
 const RightBar = ({isOpen}) => {
@@ -23,84 +24,95 @@ const RightBar = ({isOpen}) => {
     const [search, setSearch] = useState('Image');
 
     const [updateData, setUpdateData] = useState([]);
-    const [chose, setChose] = useState(['']);
+    const [chose, setChose] = useState('image');
 
-    useEffect(() => {
-        const topDiv = topRef?.current;
-        const bottomDiv = bottomRef?.current;
-
-        const shouldAutoScroll = () => {
-            if (!hasInitialized && bottomDiv) {
-                setHasInitialized(true);
-                return true;
-            }
-
-            if (!topDiv) {
-                return false;
-            }
-
-            const distanceFromBottom =
-                topDiv.scrollHeight - topDiv.scrollTop - topDiv.clientHeight;
-            return distanceFromBottom <= 100;
-        };
-
-        if (shouldAutoScroll()) {
-            setTimeout(() => {
-                bottomRef.current?.scrollIntoView({
-                    behavior: "smooth",
-                });
-            }, 100);
-        }
-    }, [topRef, hasInitialized]);
-
-
-    const imageSearchRequest = async () => {
-        console.log("searchQuery", searchQuery);
-
-        try {
-            const response = await axios.get(
-                `http://localhost:8000/api/search/text/${channelId}/${searchQuery}`// FastAPI 엔드포인트로 변경
-            );
-            console.log("Response from FastAPI: ", response);
-            if (response.status === 200) {
-                setUpdateData(response.data.similar_images); // 데이터 설정
-                setSearchQuery(""); // 검색창 초기화
-            }
-        } catch (error) {
-            console.error("Error in fetching data", error);
-        }
+    const { socket } = useSocket()
+    const components = {
+        image: <CellComponent />,
+        text: <TextSearchComponent />,
+        summary: <SummaryComponent />
     }
 
-
-    const enter_event = (e) => {
-        if (e.key === "Enter" && !e.shiftKey) {
-            e.preventDefault();
-            imageSearchRequest();
-        }
-    };
+    // useEffect(() => {
+    //     const topDiv = topRef?.current;
+    //     const bottomDiv = bottomRef?.current;
+    //
+    //     const shouldAutoScroll = () => {
+    //         if (!hasInitialized && bottomDiv) {
+    //             setHasInitialized(true);
+    //             return true;
+    //         }
+    //
+    //         if (!topDiv) {
+    //             return false;
+    //         }
+    //
+    //         const distanceFromBottom =
+    //             topDiv.scrollHeight - topDiv.scrollTop - topDiv.clientHeight;
+    //         return distanceFromBottom <= 100;
+    //     };
+    //
+    //     if (shouldAutoScroll()) {
+    //         setTimeout(() => {
+    //             bottomRef.current?.scrollIntoView({
+    //                 behavior: "smooth",
+    //             });
+    //         }, 100);
+    //     }
+    // }, [topRef, hasInitialized]);
+    //
+    //
+    // const imageSearchRequest = async () => {
+    //     console.log("searchQuery", searchQuery);
+    //
+    //     try {
+    //         const response = await axios.get(
+    //             `http://localhost:8000/api/search/text/${channelId}/${searchQuery}`// FastAPI 엔드포인트로 변경
+    //         );
+    //         console.log("Response from FastAPI: ", response);
+    //         if (response.status === 200) {
+    //             setUpdateData(response.data.similar_images); // 데이터 설정
+    //             setSearchQuery(""); // 검색창 초기화
+    //         }
+    //     } catch (error) {
+    //         console.error("Error in fetching data", error);
+    //     }
+    // }
+    //
+    //
+    // const enter_event = (e) => {
+    //     if (e.key === "Enter" && !e.shiftKey) {
+    //         e.preventDefault();
+    //         imageSearchRequest();
+    //     }
+    // };
 
     console.log("updateData", updateData)
+
+
     // 요약 파일 목록을 가져오는 함수
-    const fetchSummaryFiles = async () => {
-        try {
-            const response = await axios.get(
-                `${process.env.REACT_APP_FASTAPI_URL}/${channelId}/summaryFile`
-            );
-            const summaryData = response.data;
-            console.error("요약 파일 목록 불러오기 성공: ", summaryData);
-            setSummaries(summaryData); // 요약 데이터 상태 업데이트
-        } catch (error) {
-            console.error("요약 파일 목록 불러오기 실패: ", error);
-        }
-    };
+    // const fetchSummaryFiles = async () => {
+    //     try {
+    //         const response = await axios.get(
+    //             `${process.env.REACT_APP_FASTAPI_URL}/${channelId}/summaryFile`
+    //         );
+    //         const summaryData = response.data;
+    //         console.error("요약 파일 목록 불러오기 성공: ", summaryData);
+    //         setSummaries(summaryData); // 요약 데이터 상태 업데이트
+    //     } catch (error) {
+    //         console.error("요약 파일 목록 불러오기 실패: ", error);
+    //     }
+    // };
+    //
+    // useEffect(() => {
+    //     fetchSummaryFiles(); // 컴포넌트가 마운트될 때 요약 데이터 가져오기
+    // }, [channelId]);
+    //
+    // const onClickSummary = () => {
+    //     setSearch("Summary");
+    // };
 
-    useEffect(() => {
-        fetchSummaryFiles(); // 컴포넌트가 마운트될 때 요약 데이터 가져오기
-    }, [channelId]);
-
-    const onClickSummary = () => {
-        setSearch("Summary");
-    };
+    if (!socket) return;
 
 
     return (
@@ -111,7 +123,7 @@ const RightBar = ({isOpen}) => {
             </div>
             <div className="flex mt-2">
                 <input
-                    onClick={() => setChose("image")} type="radio" id={"image"} value="이미지" name="searchOption"
+                    onClick={() => setChose("image")} type="radio" id={"image"} value="이미지" name="searchOption" defaultChecked={true}
                     className="mr-2"
                 />
                 <label className="mr-4 text-white" htmlFor="image">
@@ -124,59 +136,17 @@ const RightBar = ({isOpen}) => {
                 <label className="mr-4 text-white" htmlFor="text">
                     텍스트
                 </label>
-                <input
-                    onClick={() => setChose("summary")} type="radio" id={"summary"} name="searchOption"
-                    className="mr-2 "
-                />
-                <label className="text-white" htmlFor="summary">
+
+                <input onClick={() => setChose("summary")} type="radio" id={"summary"} value="요약" name="searchOption"
+                       className="mr-2"/>
+                <label style={{ color: "white" }} htmlFor="summary">
                     요약
-                <label style={{ color: "white" }} htmlFor="radioSummary">
-                    <input onClick={onClickSummary} type="radio" id="radioSummary" name="search" value="요약" /> 요약
                 </label>
             </div>
-            {chose !== "summary" ?
-                <div className="relative h-10 w-full">
-                    <input
-                        type="text"
-                        value={searchQuery}
-                        onKeyDown={enter_event}
-                        onChange={(e) => setSearchQuery(e.target.value)}
-                        placeholder="검색어를 입력하세요."
-                        className="border-2 border-gray-300 rounded-md p-2 mb-10 mx-5 w-[90%] bg-right-8 bg-center bg-no-repeat bg-contain"
-                        ref={topRef}
-                    />
-                    <Button
-                        className="border-none absolute right-[5%] bottom-[10%] top-[5%]"
-                        onClick={imageSearchRequest}
-                    >
-                        <Search/>
-                    </ Button>
-                </div>
-                : null
-            }
             <div className="w-full flex-grow mx-3 mt-10 overflow-y-auto scrollbar-hidden relative">
-                {chose === "image" ? (
-                    <CellComponent updateData={updateData || false}/>
-                ) : chose === "text" ? (
-                    <TextSearchComponent updateData={updateData || false}/>
-                ) : chose === "summary" ? (
-                    <SummaryComponent channelId={channelId || false}/>
-                ) : null
-                }
+                {components[chose]}
             </div>
             <div ref={bottomRef}></div>
-
-            {/* 요약  */}
-            {search === "Summary" && (
-                <div id="summary-display">
-                    <h2 className="text-white text-center">요약 목록</h2>
-                    <ul>
-                        {summaries.map((summary) => (
-                            <li className="text-white bg-gray-500" key={summary.summary_id}>{summary.summary_text}</li>
-                        ))}
-                    </ul>
-                </div>
-            )}
         </div>
     );
 }
