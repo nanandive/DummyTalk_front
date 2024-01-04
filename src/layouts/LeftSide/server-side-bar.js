0751ff794bb6d5c9859dc84ac3682d01da8592f9
heski0709 <heski0709@gmail.com>
2024-01-04 12:50:26 +0900

    컴포넌트 이름 변경 및 serverData 전역으로 관리하도록 수정

--- src/layouts/LeftSide/server-side-bar.js
+++ src/layouts/LeftSide/server-side-bar.js
@@ -1,39 +1,40 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import Channels from "src/components/channel/Channel";
 import Friends from "src/components/channel/Friends";
 import ChannelHeader from "src/components/channel/channel-header";
 import UserSetting from "src/components/channel/user-settings";
+import { useServerData } from "src/components/hooks/use-server-data";
 import { useUrlQuery } from "src/components/hooks/use-url-query";
 import { Separator } from "src/components/ui/separator";
 
-function LeftSide() {
+function ServerSideBar() {
     const query = useUrlQuery();
     const serverId = query.get("server");
-    const [serverData, setServerData] = useState(null);
-    const { state } = useLocation();
+    const { data, setData } = useServerData()
+
     useEffect(() => {
         const fetchServerDetail = async () => {
             try {
                 const response = await axios.get(
                     `${process.env.REACT_APP_API_URL}/server/${serverId}`
                 );
-                setServerData(response.data);
+
+                setData(response.data);
             } catch (error) {
                 console.log("서버 불러오기 실패", error);
             }
         };
 
         fetchServerDetail();
-    }, [serverId, state]);
+    }, [serverId, setData]);
 
-    if (!serverId || !serverData) return null;
+    if (!serverId || !data) return null;
 
     return (
         <section className="w-60 min-w-[240px] h-full bg-[#112033] flex flex-col">
-            <ChannelHeader server={serverData} />
-            <Channels server={serverData} />
+            <ChannelHeader server={data} />
+            <Channels server={data} />
             <Separator className="my-4 bg-black" />
             <Friends className=" overflow-y-scroll" />
             <Separator className="my-4 bg-black" />
@@ -42,4 +43,4 @@ function LeftSide() {
     );
 }
 
-export default LeftSide;
+export default ServerSideBar;
