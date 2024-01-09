a8fd1de5c011490ea794de11f00e863d698bd519
haeunyy <ibbihe@gmail.com>
2024-01-09 18:05:10 +0900

    CellComponent : api 경로 오타 수정

--- src/layouts/RightSide/component/CellComponent.js
+++ src/layouts/RightSide/component/CellComponent.js
@@ -73,7 +73,7 @@ const CellComponent = () => {
         console.log("searchQuery", searchQuery);
         try {
             const response = await axios.get(
-                `http://localhost:8000/api/image/searcb/${channelId}/${searchQuery}`// FastAPI 엔드포인트로 변경
+                `http://localhost:8000/api/image/search/${channelId}/${searchQuery}`// FastAPI 엔드포인트로 변경
             );
             console.log("Response from FastAPI: ", response);
             if (response.status === 200) {
