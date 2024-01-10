7a791b02a5aab21f59a852b3c7329e3f2d202c14
haeunyy <ibbihe@gmail.com>
2024-01-10 13:03:52 +0900

    CellComponent : 스크롤 over 수정

--- src/layouts/RightSide/component/CellComponent.js
+++ src/layouts/RightSide/component/CellComponent.js
@@ -110,11 +110,10 @@ const CellComponent = () => {
                     <Search/>
                 </ Button>
             </div>
-            {/*<div*/}
-            {/*    className="overflow-y-auto scroll-smooth h-[670px] pt-6 text-amber-400 px-2 py-3 grid grid-cols-3 gap-5 w-full">*/}
-                <div className="grid grid-cols-3 gap-5 pt-6 px-2">
+            <div
+                className="overflow-y-auto scroll-smooth h-[570px] pt-6 text-amber-400 px-2 py-3 grid grid-cols-3 gap-5 w-full">
                 {data && data.map((img, index) => (
-                    <div key={index} className="relative aspect-w-3 aspect-h-4"
+                    <div key={index} className="aspect-w-3 aspect-h-4"
                          onClick={(e) => handleDownload(!img.imagePath ? img : null)}
                     >
                         <img
